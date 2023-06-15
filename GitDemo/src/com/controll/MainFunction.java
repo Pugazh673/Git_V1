74911a3ae570a8cdc01e6d978bb1a89b7bcf2875
Admin <Admin@G570-Lap>
2023-06-15 13:12:37 +0530

    This is Master

---
+++ GitDemo/src/com/controll/MainFunction.java
@@ -0,0 +1,17 @@
+package com.controll;
+
+import java.util.Iterator;
+
+public class MainFunction {
+
+	
+	
+	
+	public static void main(String[] args) {
+	for(int i=0;i<=5;i++) {
+		System.out.println("This is First ");
+	}
+
+	}
+
+}
